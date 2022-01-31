c7c8eb6d1ee6d1621c3558d9a08aba8569c3ffbc
AlexOlga <bulbenko@yandex.ru>
2022-01-31 19:34:16 +0300

    fix: for brackets ||

--- src/index.js
+++ src/index.js
@@ -6,26 +6,25 @@ if (bracketsConfig.length==0){return false};
  let newObj={};
  bracketsConfig.forEach((item)=>{
    newObj[item[0]]=item[1]});
-   //console.log('newObj',newObj);
+   console.log('newObj',newObj);
 let stek=[];
 for (let i=0; i<str.length; i++ ){
-  //console.log(`${i} in ${str.length}`);
+  console.log(`${i} in ${str.length}`);
 let item=str[i];
-//console.log('item', item);
-  if (open1.includes(item)) { stek.push(item);
-  //console.log('stak+',stek );
-  }
-    else {
-     // console.log('stak-',stek );
+console.log('item', item);
+  if ((open1.includes(item) && (item != newObj[item] )) || ((item==newObj[item]) && (!stek.includes(item))) ){ stek.push(item);
+  console.log('stak+',stek );
+  } else {
+      console.log('stak-',stek );
       if (stek.length==0){
-     //   console.log('stak pust');
+     console.log('stak pust');
         return false} ;
 
         let n=stek[stek.length-1];
-       // console.log('stak posl', n);
-        //console.log ('ob', newObj[n]);
+       console.log('stak posl', n);
+        console.log ('ob', newObj[n]);
         if (item==newObj[n]) {stek.pop();
-         // console.log('stak del',stek );
+         console.log('stak del',stek );
         } else {return false};
       }
     }
