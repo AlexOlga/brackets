fd77f730723ad5264f9aee56573cf4d9b4355d09
AlexOlga <bulbenko@yandex.ru>
2022-01-31 19:40:56 +0300

    fix: for test 7

--- src/index.js
+++ src/index.js
@@ -1,33 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-  if (str==''){return true};
+if (str==''){return true};
 if (bracketsConfig.length==0){return false};
  const  open1=bracketsConfig.map((item)=>{return item[0]});
  let newObj={};
  bracketsConfig.forEach((item)=>{
    newObj[item[0]]=item[1]});
-   console.log('newObj',newObj);
+  // console.log('newObj',newObj);
 let stek=[];
 for (let i=0; i<str.length; i++ ){
-  console.log(`${i} in ${str.length}`);
+ // console.log(`${i} in ${str.length}`);
 let item=str[i];
-console.log('item', item);
+//console.log('item', item);
   if ((open1.includes(item) && (item != newObj[item] )) || ((item==newObj[item]) && (!stek.includes(item))) ){ stek.push(item);
-  console.log('stak+',stek );
+ // console.log('stak+',stek );
   } else {
-      console.log('stak-',stek );
+    //  console.log('stak-',stek );
       if (stek.length==0){
-     console.log('stak pust');
+    // console.log('stak pust');
         return false} ;
 
         let n=stek[stek.length-1];
-       console.log('stak posl', n);
-        console.log ('ob', newObj[n]);
+      // console.log('stak posl', n);
+        //console.log ('ob', newObj[n]);
         if (item==newObj[n]) {stek.pop();
-         console.log('stak del',stek );
+        // console.log('stak del',stek );
         } else {return false};
       }
     }
-    if (stek.length==0) {return true};
+    if (stek.length==0) {return true} else {return false};
 }
 
