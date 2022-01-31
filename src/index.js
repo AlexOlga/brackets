ebb767d0fc3d6dce569c44fa68e5fd1fb03ebb39
AlexOlga <bulbenko@yandex.ru>
2022-01-31 19:49:05 +0300

    refactor: code

--- src/index.js
+++ src/index.js
@@ -1,31 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // проверка коректности условий
 if (str==''){return true};
 if (bracketsConfig.length==0){return false};
+//создаем массив открытых скобок
  const  open1=bracketsConfig.map((item)=>{return item[0]});
+ // задаем объект ( => )
  let newObj={};
- bracketsConfig.forEach((item)=>{
-   newObj[item[0]]=item[1]});
-  // console.log('newObj',newObj);
+ bracketsConfig.forEach((item)=>{ newObj[item[0]]=item[1]});
+ 
 let stek=[];
 for (let i=0; i<str.length; i++ ){
- // console.log(`${i} in ${str.length}`);
-let item=str[i];
-//console.log('item', item);
-  if ((open1.includes(item) && (item != newObj[item] )) || ((item==newObj[item]) && (!stek.includes(item))) ){ stek.push(item);
- // console.log('stak+',stek );
-  } else {
-    //  console.log('stak-',stek );
-      if (stek.length==0){
-    // console.log('stak pust');
-        return false} ;
-
-        let n=stek[stek.length-1];
-      // console.log('stak posl', n);
-        //console.log ('ob', newObj[n]);
-        if (item==newObj[n]) {stek.pop();
-        // console.log('stak del',stek );
-        } else {return false};
+    let item=str[i];
+    //является ли скобка открытой, если открытая и закрытая скобки одинаковые, открывалась ли уже такая
+    if ((open1.includes(item) && (item != newObj[item] )) || ((item==newObj[item]) && (!stek.includes(item))) )
+    { stek.push(item);  }
+    else {
+      // скобка закрывающаяся
+         if (stek.length==0){return false} ;
+          let n=stek[stek.length-1];
+             if (item==newObj[n]) {stek.pop();} 
+             else {return false};
       }
     }
     if (stek.length==0) {return true} else {return false};
